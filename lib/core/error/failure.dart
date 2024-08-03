976f1d569ce9e0832be50fa332bacf23c3dc6a24
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2024-08-03 19:22:01 +0300

    add new way to handle failure cases ..

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 abstract class FailureHandler {
   final String message;
   final bool status;
@@ -13,6 +15,77 @@ class FailureCase extends FailureHandler {
   FailureCase({
     required super.message,
     required super.status,
-    required super.failuresCases ,
+    required super.failuresCases,
   });
+}
+
+class HandleInternetConnectionError extends FailureHandler {
+  HandleInternetConnectionError(
+      {required super.message,
+      required super.status,
+      required super.failuresCases});
+
+  factory HandleInternetConnectionError.fromDioExceptionType(
+      {DioExceptionType? dioError, FailureHandler? model}) {
+    switch (dioError) {
+      case DioExceptionType.badCertificate:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.connectionError:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.connectionTimeout:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.sendTimeout:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.receiveTimeout:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.cancel:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.badResponse:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+      case DioExceptionType.unknown:
+        return HandleInternetConnectionError(
+          message: model!.message,
+          status: model.status,
+          failuresCases: model.failuresCases,
+        );
+
+      // return HandleInternetConnectionError(
+      //     "Check your Internet connection and try again");
+
+      default:
+        return HandleInternetConnectionError(
+          message: "Some thing is wrong , please try again later",
+          status: false,
+          failuresCases: [],
+        );
+    }
+  }
 }
